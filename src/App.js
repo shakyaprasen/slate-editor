b889929eb08d475217f5671acc97fe1d00ff4241
shakyaprasen <kaji.prasen@gmail.com>
2020-03-27 11:29:21 +0545

    Added readonly text to the editor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,175 @@
-import React from 'react';
-import logo from './logo.svg';
+// Import React dependencies.
+import React, { useCallback, useMemo, useState } from "react";
+// Import the Slate editor factory.
+import { createEditor, Transforms, Editor, Text } from 'slate'
+
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
 import './App.css';
 
-function App() {
+// Define our own custom set of helpers.
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.bold === true,
+      universal: true,
+    })
+
+    return !!match
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === 'code',
+    })
+
+    return !!match
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor)
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true },
+      { match: n => Text.isText(n), split: true }
+    )
+  },
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor)
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : 'code' },
+      { match: n => Editor.isBlock(editor, n) }
+    )
+  },
+};
+
+const App = () => {
+  const editor = useMemo(() => withReact(createEditor()), [])
+
+  const { isVoid } = editor;
+
+  editor.isVoid = element => {
+    return element.type === 'readOnly' ? true : isVoid(element);
+  };
+
+  const [value, setValue] = useState([
+    {
+      type: 'readOnly',
+      children: [{ text: 'A line of text to be translated.' }],
+    },
+    {
+      type: 'paragraph',
+      children: [{ text: '' }],
+    },
+    {
+      type: 'readOnly',
+      children: [{ text: 'A line of text in third paragraph.' }],
+    },
+    {
+      type: 'paragraph',
+      children: [{ text: '' }],
+    },
+  ])
+
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      case 'readOnly':
+        return <ReadOnlyElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
+
+  const renderLeaf = useCallback(props => {
+    return <Leaf {...props} />
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    // Add a toolbar with buttons that call the same methods.
+    <Slate
+      onChange={value => {
+        setValue(value)
+
+        // Save the value to Local Storage.
+        const content = JSON.stringify(value)
+        localStorage.setItem('content', content)
+      }}
+      editor={editor} 
+      value={value} 
+    >
+      <div>
+        <button
+          onMouseDown={event => {
+            event.preventDefault()
+            CustomEditor.toggleBoldMark(editor)
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Bold
+        </button>
+        <button
+          onMouseDown={event => {
+            event.preventDefault()
+            CustomEditor.toggleCodeBlock(editor)
+          }}
+        >
+          Code Block
+        </button>
+      </div>
+      <Editable
+        editor={editor}
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        onKeyDown={event => {
+          if (!event.ctrlKey) {
+            return
+          }
+
+          switch (event.key) {
+            case '`': {
+              event.preventDefault()
+              CustomEditor.toggleCodeBlock(editor)
+              break
+            }
+
+            case 'b': {
+              event.preventDefault()
+              CustomEditor.toggleBoldMark(editor)
+              break
+            }
+          }
+        }}
+      />
+    </Slate>
+  )
 }
 
+
+const CodeElement = props => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+}
+const DefaultElement = props => {
+  return <p {...props.attributes}>{props.children}</p>
+}
+
+const ReadOnlyElement = props => {
+  return <div {...props.attributes}><p contentEditable={false}>{props.element.children[0].text}</p>{props.children}</div>
+}
+// Define a React component to render leaves with bold text.
+const Leaf = props => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
+    >
+      {props.children}
+    </span>
+  )
+}
 export default App;
